c8d2964e1089bc9033605385e7c15d424db0eb1e
ivashchvlad <ivashchvlad@gmail.com>
2020-06-14 13:59:29 +0400

    photocard style

--- src/PhotoCard.js
+++ src/PhotoCard.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import './PhotoCard.css'
 
-
-function PhotoCard({ cardId }) {
+function PhotoCard({ cardId, open }) {
     const [card, setCard] = useState();
     useEffect(() => {
         axios.get(`https://boiling-refuge-66454.herokuapp.com/images/${cardId}`)
@@ -11,36 +11,45 @@ function PhotoCard({ cardId }) {
             })
             .catch(e => console.log(e))
     }, [cardId])
-    return card ? 
-        (<div className="card">
-            <div calssName="card__close">
-                <button>X</button>
-            </div>
-            <div className="card__photo">
-                <img src={card.url} alt="card.id" />
-            </div>
-            <div className="card__comments">
-                {
-                    card.comments.map(comment => {
-                        return (
-                            <div className="comment">
-                                <h4 className="comment__date">
-                                    {comment.date}
-                                </h4>
-                                <h4 className="comment_text">
-                                    {comment.text}
-                                </h4>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-            <div className="card__commentform">
-                <input type="text" name="name" id="name" placeholder="Ваше имя"/>
-                <input type="text" name="comment" id="comment" placeholder="Ваш комментарий"/>
-                <button>Оставить комменатрий</button>
-            </div>
-        </div>) : null
+
+    const close = () => {
+        setCard('');
+    }
+
+    return card ?
+        (
+            <div className="popup">
+                <div className="popup__background" onClick={close}></div>
+                <div className="card">
+                    <div className="card__close">
+                        <button onClick={close}>x</button>
+                    </div>
+                    <div className="card__photo">
+                        <img src={card.url} alt="card.id" />
+                    </div>
+                    <div className="card__comments">
+                        {
+                            card.comments.map(comment => {
+                                return (
+                                    <div className="comment">
+                                        <h4 className="comment__date">
+                                            {comment.date}
+                                        </h4>
+                                        <h4 className="comment__text">
+                                            {comment.text}
+                                        </h4>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                    <div className="card__commentform">
+                        <input type="text" name="name" id="name" placeholder="Ваше имя" />
+                        <input type="text" name="comment" id="comment" placeholder="Ваш комментарий" />
+                        <button>Оставить комменатрий</button>
+                    </div>
+                </div>
+            </div>) : null
 }
 
 export default PhotoCard
