bec316bcea685c829794ac8c42c639e018f0bf83
ivashchvlad <ivashchvlad@gmail.com>
2020-06-15 20:42:00 +0400

    photocard add comment

--- src/App.js
+++ src/App.js
@@ -5,48 +5,47 @@ import './App.css'
 import PhotoCard from './PhotoCard'
 
 function Avito() {
-    const [cards, setCards] = useState([])
-    const [selectedCardId, setSelectedCardId] = useState();
-    useEffect(() => {
-        axios.get('https://boiling-refuge-66454.herokuapp.com/images')
-        .then(res => setCards(res.data))
-        .catch(e => console.log(e))
-    }, [])
+  const [cards, setCards] = useState([])
+  const [selectedCardId, setSelectedCardId] = useState();
+  useEffect(() => {
+    axios.get('https://boiling-refuge-66454.herokuapp.com/images')
+      .then(res => setCards(res.data))
+      .catch(e => console.log(e))
+  }, [])
 
-    const openCard = (e) => {
-      console.log(e);
-      setSelectedCardId(e.target.name);
-    }
+  const openCard = (e) => {
+    setSelectedCardId(e.target.name);
+  }
 
-    return (
-        <>
-        <PhotoCard cardId={selectedCardId}/>
-        <div className="container">
-            <header className="header">
-              <h1 className="header__title">Test App</h1>
-            </header>
-            <main className="main">
-                {
-                    cards.length ? cards.map(card => 
-                      (<div className="main__image">
-                        <img
-                          name={card.id}
-                          src={card.url} 
-                          alt='nice view'
-                          key={card.id} 
-                          onClick={openCard}
-                        />
-                      </div>)
-                    ) :
-                    'loading...'
-                }
-            </main>
-            <footer className="footer">
-              <h3 className="footer__copyright">©2018-2019</h3>
-            </footer>
-        </div>
-      </>
-    )
+  return (
+    <>
+      <PhotoCard cardId={selectedCardId} key="1"/>
+      <div className="container">
+        <header className="header">
+          <h1 className="header__title">Test App</h1>
+        </header>
+        <main className="main">
+          {
+            cards.length ? cards.map(card =>
+              (<div className="main__image" key={card.id}>
+                <img
+                  name={card.id}
+                  src={card.url}
+                  alt='nice view'
+                  key={card.id}
+                  onClick={openCard}
+                />
+              </div>)
+            ) :
+              'loading...'
+          }
+        </main>
+        <footer className="footer">
+          <h3 className="footer__copyright">©2018-2019</h3>
+        </footer>
+      </div>
+    </>
+  )
 }
 
 export default Avito
--- src/PhotoCard.js
+++ src/PhotoCard.js
@@ -4,6 +4,8 @@ import './PhotoCard.css'
 
 function PhotoCard({ cardId, open }) {
     const [card, setCard] = useState();
+    const [name, setName] = useState('')
+    const [comment, setComment] = useState('');
     useEffect(() => {
         axios.get(`https://boiling-refuge-66454.herokuapp.com/images/${cardId}`)
             .then(res => {
@@ -14,6 +16,26 @@ function PhotoCard({ cardId, open }) {
 
     const close = () => {
         setCard('');
+    }
+
+    const handleChange = (e) => {
+        switch(e.target.name) {
+            case 'name': setName(e.target.value); break;
+            case 'comment': setComment(e.target.value); break;
+            default: return;
+        }
+    }   
+
+    const addComment = () => {
+        axios.post(`https://boiling-refuge-66454.herokuapp.com/images/${cardId}/comments`, 
+        { name: name, comment: comment })
+        .then((res) => {
+            if (res.status === 204){
+                setName('');
+                setComment('');
+            }
+        })
+        .catch(e => console.log(e.message));
     }
 
     return card ?
@@ -44,9 +66,23 @@ function PhotoCard({ cardId, open }) {
                         }
                     </div>
                     <div className="card__commentform">
-                        <input type="text" name="name" id="name" placeholder="Ваше имя" />
-                        <input type="text" name="comment" id="comment" placeholder="Ваш комментарий" />
-                        <button>Оставить комменатрий</button>
+                        <input 
+                            type="text" 
+                            name="name" 
+                            id="name" 
+                            placeholder="Ваше имя"
+                            value={name}
+                            onChange={handleChange} 
+                        />
+                        <input 
+                            type="text" 
+                            name="comment" 
+                            id="comment" 
+                            placeholder="Ваш комментарий"
+                            value={comment}
+                            onChange={handleChange}
+                        />
+                        <button onClick={addComment}>Оставить комменатрий</button>
                     </div>
                 </div>
             </div>) : null
