57fff4bc6f2f9873602f0973b3a68f40ebdebdca
ivashchvlad <ivashchvlad@gmail.com>
2020-06-17 21:45:52 +0400

    Updates

---
+++ precache-manifest.973e6db998b87d95d6ad57715f3955a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5046b8159923e468f5f9a1a68f7ada5",
+    "url": "/avitoTest/index.html"
+  },
+  {
+    "revision": "7e9db7486a29d3d22285",
+    "url": "/avitoTest/static/css/main.ecd9ef1c.chunk.css"
+  },
+  {
+    "revision": "b53e84fe8afb9e48a744",
+    "url": "/avitoTest/static/js/2.dd1bd9fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/avitoTest/static/js/2.dd1bd9fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9db7486a29d3d22285",
+    "url": "/avitoTest/static/js/main.992d5a29.chunk.js"
+  },
+  {
+    "revision": "369694fdc85019e67c3d",
+    "url": "/avitoTest/static/js/runtime-main.21a16e94.js"
+  }
+]);
